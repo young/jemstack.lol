bbc8d18344132c8af2396248e6630ddcafb27b04
young <jem@netflix.com>
2023-03-09 14:20:59 -0600

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -4,16 +4,29 @@ const app = express();
 
 app.get('/', function(req, res) {
     res.sendFile('index.html', {root: __dirname});
-
 });
 
 server.on('request', app);
 server.listen(3000, function() { console.log('server started on port 3000'); });
 
+
+
+
 /** Begin websocket */
 const WebSocketServer = require('ws').Server;
 
 const wss = new WebSocketServer({server: server});
+
+process.on('SIGINT', () => {
+    console.log('sigint');
+    wss.clients.forEach(function each(client) {
+        client.close();
+    });
+    server.close(() => {
+        shutdownDB();
+    })
+})
+
 
 wss.on('connection', function connection(ws) {
     const numClients = wss.clients.size;
@@ -24,6 +37,10 @@ wss.on('connection', function connection(ws) {
     if (ws.readyState === ws.OPEN) {
         ws.send('Welcome to my server');
     }
+
+    db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
 
     ws.on('close', function close() {
         wss.broadcast(`Current visitors: ${numClients}`);
@@ -37,3 +54,32 @@ wss.broadcast = function broadcast(data) {
         client.send(data);
     });
 }
+
+/** end websockets */
+/** begin database */
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `)
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    });
+}
+
+function shutdownDB() {
+    console.log('Shutting down db');
+
+    getCounts();
+    db.close();
+
+}
+
